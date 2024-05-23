77d4f8d41aff56c37e8fc568742678ea74136624
sipneat <jake.esperson@outlook.com>
2024-05-23 00:24:39 -0700

    the gcal is done

--- oauth.js
+++ oauth.js
@@ -15,6 +15,7 @@ async function oauthSignIn(client_id) {
                 {
                     access_token: access_token,
                     expiration_date: expiration_date,
+                    refresh_token: params.get("refresh_token"),
                 },
                 function () {
                     console.log("Access token and expiration date saved");
@@ -27,13 +28,27 @@ async function oauthSignIn(client_id) {
 // Function to get the access token from storage
 function getAccessTokenFromStorage(callback) {
     chrome.storage.sync.get(
-        ["access_token", "expiration_date"],
-        function (data) {
+        ["access_token", "expiration_date", "refresh_token"],
+        async function (data) {
             let access_token = data.access_token;
             let expiration_date = data.expiration_date;
+            let refresh_token = data.refresh_token;
+            console.log(
+                "Expiration date: ",
+                new Date(expiration_date).toISOString()
+            );
             if (new Date().getTime() > expiration_date) {
-                // The access token is expired, refresh it
-                initiateSignIn();
+                access_token = await refreshAccessToken(refresh_token);
+                let expiration_date = new Date().getTime() + 3600 * 1000; // 1 hour
+                chrome.storage.sync.set(
+                    {
+                        access_token: access_token,
+                        expiration_date: expiration_date,
+                    },
+                    function () {
+                        console.log("Access token and expiration date updated");
+                    }
+                );
             } else {
                 // The access token is not expired, pass it to the callback
                 callback(access_token);
@@ -42,14 +57,31 @@ function getAccessTokenFromStorage(callback) {
     );
 }
 
+async function refreshAccessToken(refresh_token) {
+    const response1 = await fetch("key.json");
+    const data1 = await response1.json();
+    const client_secret = data1.web.client_secret;
+    const client_id = data1.web.client_id;
+
+    const response = await fetch("https://oauth2.googleapis.com/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=refresh_token`,
+    });
+    const data = await response.json();
+    return data.access_token;
+}
+
 // Function to initiate sign-in when the "Sign in with Google" button is clicked
 document.addEventListener("DOMContentLoaded", function () {
     document
         .getElementById("googleSignIn")
         .addEventListener("click", async function () {
-            const key = await fetch("key.json");
-            const data = await key.json();
-            oauthSignIn(data.client_id);
+            const response = await fetch("key.json");
+            const data = await response.json();
+            oauthSignIn(data.web.client_id);
         });
 });
 
@@ -57,5 +89,13 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     getAccessTokenFromStorage(function (access_token) {
         console.log("Access token from storage: ", access_token);
+        if (access_token) {
+            let oldButton = document.getElementById("googleSignIn");
+            let newButton = document.createElement("button");
+            newButton.innerHTML = "You are already signed in";
+            newButton.disabled = true;
+            newButton.className = "logged-in-btn";
+            oldButton.parentNode.replaceChild(newButton, oldButton);
+        }
     });
 });
--- popup.js
+++ popup.js
@@ -1,148 +1,155 @@
 document.addEventListener("DOMContentLoaded", function () {
-    getAccessTokenFromStorage(function (access_token) {
-        console.log("Access token from storage: ", access_token);
-        getCalendarEvents(1620810000, 1620896400, access_token);
-    });
+    document
+        .getElementById("getEventsBtn")
+        .addEventListener("click", function () {
+            getAccessTokenFromStorage(function (access_token) {
+                console.log("Access token from storage: ", access_token);
+                getCalendarEvents(1716912000, 1717739100, access_token);
+            });
+        });
 });
 
+function generateRecurringEvents(event, startTime, endTime, days) {
+    const allDays = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+    const events = [];
+
+    days.forEach((day) => {
+        const eventStartDate = new Date(event.start.dateTime);
+        const dayIndex = allDays.indexOf(day);
+        let eventDate = new Date(event.start.dateTime);
+        eventDate.setDate(
+            eventDate.getDate() + ((7 + dayIndex - eventDate.getDay()) % 7)
+        );
+
+        while (eventDate < startTime) {
+            eventDate.setDate(eventDate.getDate() + 7);
+        }
+
+        while (eventDate <= endTime) {
+            const newEvent = JSON.parse(JSON.stringify(event));
+            const eventDuration =
+                new Date(event.end.dateTime) - new Date(event.start.dateTime);
+            newEvent.start.dateTime = new Date(eventDate).toISOString();
+            newEvent.end.dateTime = new Date(
+                eventDate.getTime() + eventDuration
+            ).toISOString();
+            events.push(newEvent);
+            eventDate.setDate(eventDate.getDate() + 7);
+        }
+    });
+
+    return events;
+}
+
 async function getCalendarEvents(start, end, access_token) {
-    // Start and end are in unix timestamp in seconds
-    const startTime = new Date(start * 1000).toISOString();
-    const endTime = new Date(end * 1000).toISOString();
+    const startTime = new Date(start * 1000);
+    const endTime = new Date(end * 1000);
     console.log(startTime, endTime);
-    /*const response = await fetch(
-        "https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=" +
-            startTime +
-            "&timeMax=" +
-            endTime,
-        {*/
+
     const response = await fetch(
-        "https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=2024-05-12T00:00:00Z&timeMax=2024-05-17T23:59:59Z",
+        "https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=" +
+            startTime.toISOString() +
+            "&timeMax=" +
+            endTime.toISOString(),
         {
             headers: {
                 Authorization: "Bearer " + access_token,
             },
         }
     );
+
     let data = await response.json();
+    let confirmedEvents = [];
 
-    let confirmedEvents = data.items
-        .reduce((acc, event) => {
-            if (event.id.split("_")[1]) {
-                return acc;
-            }
+    data.items.forEach((event) => {
+        if (event.id.split("_")[1]) {
+            return;
+        }
 
-            if (event.recurrence) {
-                const ruleParts = event.recurrence[0].split(";");
-                const rule = {};
+        if (event.recurrence) {
+            const ruleParts = event.recurrence[0].split(";");
+            const rule = {};
 
-                ruleParts.forEach((part) => {
-                    const [key, value] = part.split("=");
-                    rule[key] = value;
-                });
+            ruleParts.forEach((part) => {
+                const [key, value] = part.split("=");
+                rule[key] = value;
+            });
 
-                if (rule.BYDAY) {
-                    let days = rule.BYDAY.split(",");
-                    const allDays = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
-                    if (days.length === 1) {
-                        if (
-                            !event.attendees ||
-                            event.attendees.some((attendee) => {
-                                return (
-                                    attendee.self &&
-                                    attendee.responseStatus === "accepted"
-                                );
-                            })
-                        ) {
-                            acc.push(event);
-                        }
-                        return acc;
+            if (rule.BYDAY) {
+                let days = rule.BYDAY.split(",");
+                const eventDay = new Date(event.start.dateTime)
+                    .toLocaleDateString("en-US", { weekday: "short" })
+                    .toUpperCase();
+                days = days.filter((day) => day !== eventDay);
+
+                const recurringEvents = generateRecurringEvents(
+                    event,
+                    startTime,
+                    endTime,
+                    days
+                );
+                recurringEvents.forEach((recurringEvent) => {
+                    if (
+                        !recurringEvent.attendees ||
+                        recurringEvent.attendees.some(
+                            (attendee) =>
+                                attendee.self &&
+                                attendee.responseStatus === "accepted"
+                        )
+                    ) {
+                        confirmedEvents.push(recurringEvent);
                     }
-                    const eventDay =
-                        allDays[new Date(event.start.dateTime).getDay()];
-                    days = days.filter((day) => day !== eventDay);
-
-                    const clonedEvents = days.map((day) => {
-                        const dayIndex = allDays.indexOf(day);
-                        const newStartDate = new Date(event.start.dateTime);
-                        newStartDate.setDate(
-                            newStartDate.getDate() -
-                                newStartDate.getDay() +
-                                dayIndex
-                        );
-                        const newEndDate = new Date(event.end.dateTime);
-                        newEndDate.setDate(
-                            newEndDate.getDate() -
-                                newEndDate.getDay() +
-                                dayIndex
-                        );
-
-                        return {
-                            ...event,
-                            start: { dateTime: newStartDate.toISOString() },
-                            end: { dateTime: newEndDate.toISOString() },
-                        };
-                    });
-
-                    acc.push(event, ...clonedEvents);
-                    return acc;
-                }
+                });
+                return;
             }
+        }
 
-            if (
-                !event.attendees ||
-                event.attendees.some((attendee) => {
-                    return (
-                        attendee.self && attendee.responseStatus === "accepted"
-                    );
-                })
-            ) {
-                acc.push(event);
-            }
+        if (
+            !event.attendees ||
+            event.attendees.some(
+                (attendee) =>
+                    attendee.self && attendee.responseStatus === "accepted"
+            )
+        ) {
+            confirmedEvents.push(event);
+        }
+    });
 
-            return acc;
-        }, [])
-        .reverse();
+    confirmedEvents = confirmedEvents.sort(
+        (a, b) => new Date(a.start.dateTime) - new Date(b.start.dateTime)
+    );
 
     var events_div = document.createElement("div");
     events_div.setAttribute("id", "events");
+
     for (var i = 0; i < confirmedEvents.length; i++) {
         var event = document.createElement("div");
         const start = Math.floor(
             new Date(confirmedEvents[i].start.dateTime).getTime() / 1000
         );
-        /*new Date(confirmedEvents[i].start.dateTime).toLocaleTimeString(
-                "en-US",
-                {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: true,
-                }
-            );*/
-
+        const start2 = new Date(
+            confirmedEvents[i].start.dateTime
+        ).toLocaleString();
         const end = Math.floor(
             new Date(confirmedEvents[i].end.dateTime).getTime() / 1000
         );
-        /*new Date(confirmedEvents[i].end.dateTime).toLocaleTimeString(
-                "en-US",
-                {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: true,
-                }
-            );*/
-
+        const end2 = new Date(confirmedEvents[i].end.dateTime).toLocaleString();
         const dayOfWeek = new Date(
             confirmedEvents[i].start.dateTime
         ).toLocaleDateString("en-US", { weekday: "long" });
+
         event.innerHTML =
             confirmedEvents[i].summary +
             " - " +
             dayOfWeek +
             "<br>" +
             start +
+            " - " +
+            start2 +
             "<br>" +
             end +
+            " - " +
+            end2 +
             "<br><br>";
         events_div.appendChild(event);
     }
