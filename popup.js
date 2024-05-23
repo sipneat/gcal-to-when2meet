c474d16267080562735b1d175d0a137627c02ea8
sipneat <jake.esperson@outlook.com>
2024-05-22 18:19:59 -0700

    kinda

--- popup.js
+++ popup.js
@@ -11,7 +11,17 @@ async function initiateSignIn() {
     }
 }
 
-async function getCalendarEvents() {
+async function getCalendarEvents(start, end) {
+    // Start and end are in unix timestamp in seconds
+    const startTime = new Date(start * 1000).toISOString();
+    const endTime = new Date(end * 1000).toISOString();
+    console.log(startTime, endTime);
+    /*const response = await fetch(
+        "https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=" +
+            startTime +
+            "&timeMax=" +
+            endTime,
+        {*/
     const response = await fetch(
         "https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=2024-05-12T00:00:00Z&timeMax=2024-05-17T23:59:59Z",
         {
@@ -22,53 +32,112 @@ async function getCalendarEvents() {
     );
     let data = await response.json();
 
-    let seen = new Set();
-    function getEventKey(event) {
-        const date = new Date(event.start.dateTime);
-        const day = date.toLocaleDateString("en-US", { weekday: "long" });
-        console.log(day);
-        const time = date.toLocaleTimeString("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-        });
-        return `${day} ${time}`;
-    }
+    let confirmedEvents = data.items
+        .reduce((acc, event) => {
+            if (event.id.split("_")[1]) {
+                return acc;
+            }
 
-    const confirmedEvents = data.items
-        .filter((event) => {
-            return (
+            if (event.recurrence) {
+                const ruleParts = event.recurrence[0].split(";");
+                const rule = {};
+
+                ruleParts.forEach((part) => {
+                    const [key, value] = part.split("=");
+                    rule[key] = value;
+                });
+
+                if (rule.BYDAY) {
+                    let days = rule.BYDAY.split(",");
+                    const allDays = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+                    if (days.length === 1) {
+                        if (
+                            !event.attendees ||
+                            event.attendees.some((attendee) => {
+                                return (
+                                    attendee.self &&
+                                    attendee.responseStatus === "accepted"
+                                );
+                            })
+                        ) {
+                            acc.push(event);
+                        }
+                        return acc;
+                    }
+                    const eventDay =
+                        allDays[new Date(event.start.dateTime).getDay()];
+                    days = days.filter((day) => day !== eventDay);
+
+                    const clonedEvents = days.map((day) => {
+                        const dayIndex = allDays.indexOf(day);
+                        const newStartDate = new Date(event.start.dateTime);
+                        newStartDate.setDate(
+                            newStartDate.getDate() -
+                                newStartDate.getDay() +
+                                dayIndex
+                        );
+                        const newEndDate = new Date(event.end.dateTime);
+                        newEndDate.setDate(
+                            newEndDate.getDate() -
+                                newEndDate.getDay() +
+                                dayIndex
+                        );
+
+                        return {
+                            ...event,
+                            start: { dateTime: newStartDate.toISOString() },
+                            end: { dateTime: newEndDate.toISOString() },
+                        };
+                    });
+
+                    acc.push(event, ...clonedEvents);
+                    return acc;
+                }
+            }
+
+            if (
                 !event.attendees ||
                 event.attendees.some((attendee) => {
                     return (
                         attendee.self && attendee.responseStatus === "accepted"
                     );
                 })
-            );
-        })
-        .filter((event) => {
-            const key = getEventKey(event);
-            return seen.has(key) ? false : seen.add(key);
-        })
+            ) {
+                acc.push(event);
+            }
+
+            return acc;
+        }, [])
         .reverse();
 
     var events_div = document.createElement("div");
     events_div.setAttribute("id", "events");
     for (var i = 0; i < confirmedEvents.length; i++) {
         var event = document.createElement("div");
-        const start = new Date(
-            confirmedEvents[i].start.dateTime
-        ).toLocaleTimeString("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: true,
-        });
-        const end = new Date(
-            confirmedEvents[i].end.dateTime
-        ).toLocaleTimeString("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: true,
-        });
+        const start = Math.floor(
+            new Date(confirmedEvents[i].start.dateTime).getTime() / 1000
+        );
+        /*new Date(confirmedEvents[i].start.dateTime).toLocaleTimeString(
+                "en-US",
+                {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                }
+            );*/
+
+        const end = Math.floor(
+            new Date(confirmedEvents[i].end.dateTime).getTime() / 1000
+        );
+        /*new Date(confirmedEvents[i].end.dateTime).toLocaleTimeString(
+                "en-US",
+                {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                }
+            );*/
+
         const dayOfWeek = new Date(
             confirmedEvents[i].start.dateTime
         ).toLocaleDateString("en-US", { weekday: "long" });
